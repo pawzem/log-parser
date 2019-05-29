a137023c6b72257a98f9afcd4aae6c1b7ee12152
Paweł Żemła <pawzem@gmail.com>
2019-05-29 08:52:39 +0200

    print in order of occurrence

--- log-parser.py
+++ log-parser.py
@@ -13,28 +13,38 @@ def main():
     try:
         log_file = open(log_file_name, "r", encoding="utf8")
         requests = {}
+        order_counter = 0
+        occurrence_order = {}
         for line in log_file:
-            parse_line(line, requests)
-        durations = calculate_durations(requests)
+            parse_line(line, requests, occurrence_order, order_counter)
+            order_counter += 1
+        sorted_requests = sort_requests(requests, occurrence_order)
+        durations = calculate_durations(sorted_requests)
 
         print("id\tcorrelation_id\tduration")
         current_id = 0
-        for correlation_id, duration in durations.items():
+        for correlation_id, duration in durations:
             print("{}\t{}\t{}".format(current_id, correlation_id, duration))
             current_id += 1
     finally:
         log_file.close()
 
 
-def calculate_durations(requests):
-    durations = {}
-    for correlation_id, times in requests.items():
+def sort_requests(requests, occurrence_order):
+    sorted_requests = list(requests.items())
+    sorted_requests.sort(key=lambda k: occurrence_order[k[0]])
+    return sorted_requests
+
+
+def calculate_durations(sorted_requests):
+    durations = []
+    for correlation_id, times in sorted_requests:
         duration = max(times) - min(times)
-        durations[correlation_id] = duration.total_seconds()
+        durations.append((correlation_id, duration.total_seconds()))
     return durations
 
 
-def parse_line(line, requests):
+def parse_line(line, requests, occurrence_order, order_counter):
     regex = r'(.*)  INFO \[(.*),(.*),(.*),(.*)\]'
     compiled_regex = re.compile(regex)
     match = compiled_regex.search(line)
@@ -44,6 +54,7 @@ def parse_line(line, requests):
         if base is '':
             return
         if base not in requests:
+            occurrence_order[base] = order_counter
             requests[base] = (time, None)
         elif requests[base][1] is None:
             requests[base] = (requests[base][0], time)
